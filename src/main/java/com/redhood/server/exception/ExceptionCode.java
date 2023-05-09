b511cb4d36c81581b34332763e04c422378b8bc1
SEB_BE_43_남지훈 <120356702+skawlgns1@users.noreply.github.com>
2023-05-09 12:40:08 +0900

    Add files via upload

---
+++ src/main/java/com/redhood/server/exception/ExceptionCode.java
@@ -0,0 +1,26 @@
+package com.redhood.server.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404, "Member not found"),
+    MEMBER_EXISTS(409, "Member exists"),
+    EMAIL_EXISTS(409, "Email exists"),
+    PASSWORD_NOT_MATCH(404, "Password does not match"),
+    NICKNAME_EXISTS(409, "Nickname exists"),
+    QUESTION_NOT_FOUND(404, "Question not found"),
+    QUESTION_AUTHOR_NOT_MATCH(404, "The author of the question does not match"),
+    ANSWER_NOT_FOUND(404,"Answer not found"),
+    ANSWER_AUTHOR_NOT_MATCH(404,"The author of the answer does not match");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int code, String message) {
+        this.status = code;
+        this.message = message;
+    }
+}
